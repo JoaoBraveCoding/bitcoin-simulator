24f57e7b8a4258f7c6b7e1cf6b46f5ab5db110e0
JoaoBraveCoding <joao.marcal12@hotmail.com>
2018-04-09 12:08:35 +0100

    Fixed some spacing

--- echo.py
+++ echo.py
@@ -2,8 +2,6 @@
 # Miguel Matos - miguel.marques.matos@tecnico.ulisboa.pt
 # (c) 2012-2018
 
-LOG_TO_FILE=False
-
 from collections import defaultdict
 import math
 import random
@@ -16,12 +14,15 @@ import logging
 from sim import sim
 import utils
 
+LOG_TO_FILE = False
+
+
 def init():
     global nodeState
 
     # schedule execution for all nodes
     for nodeId in nodeState:
-        sim.schedulleExecution(CYCLE,nodeId)
+        sim.schedulleExecution(CYCLE, nodeId)
 
     # other things such as periodic measurements can also be scheduled
     # to schedulle periodic measurements use the following
@@ -31,64 +32,70 @@ def init():
 
 def CYCLE(myself):
     global nodeState
-    
+
     # with churn the node might be gone
     if myself not in nodeState:
         return
 
     # show progress for one node
     if myself == 0:
-        logger.info('node {} cycle {}'.format(myself,nodeState[myself][CURRENT_CYCLE]))
+        logger.info('node {} cycle {}'.format(myself, nodeState[myself][CURRENT_CYCLE]))
 
     nodeState[myself][CURRENT_CYCLE] += 1
 
     # schedule next execution
     if nodeState[myself][CURRENT_CYCLE] < nbCycles:
-        sim.schedulleExecution(CYCLE,myself)
+        sim.schedulleExecution(CYCLE, myself)
 
     # select random node to send message
     # assume global view
     if random.random() <= probBroadcast:
         target = random.choice(nodeState.keys())
-        sim.send(HELLO, target, myself, "hello, i am {}".format(myself),"hello")
+        sim.send(HELLO, target, myself, "hello, i am {}".format(myself), "hello")
 
-        nodeState[myself][MSGS_SENT]+=1
+        nodeState[myself][MSGS_SENT] += 1
 
-def HELLO(myself, source, msg1,msg2):
+
+def HELLO(myself, source, msg1, msg2):
     global nodeState
 
-    logger.info("Node {} Received {} from {} with {}".format(myself,msg1,source,msg2))
+    logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, msg2))
     nodeState[myself][MSGS_RECEIVED] += 1
-    sim.send(HELLO_REPLY, source, myself, "hello reply, i am {}".format(myself),"hello to you too")
+    sim.send(HELLO_REPLY, source, myself, "hello reply, i am {}".format(myself), "hello to you too")
 
 
-def HELLO_REPLY(myself, source, msg1,msg2):
+def HELLO_REPLY(myself, source, msg1, msg2):
     global nodeState
 
-    logger.info("Node {} Received {} from {} with {}".format(myself,msg1,source,msg2))
+    logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, msg2))
     nodeState[myself][MSGS_RECEIVED] += 1
+
 
 def wrapup():
     global nodeState
     logger.info("Wrapping up")
     logger.info(nodeState)
 
-    receivedMessages = map(lambda x : nodeState[x][MSGS_RECEIVED], nodeState)
-    sentMessages = map(lambda x : nodeState[x][MSGS_SENT], nodeState)
+    receivedMessages = map(lambda x: nodeState[x][MSGS_RECEIVED], nodeState)
+    sentMessages = map(lambda x: nodeState[x][MSGS_SENT], nodeState)
 
     # gather some stats, see utils for more functions
     logger.info("receivedMessages {}".format(receivedMessages))
-    logger.info("receivedMessages min: {}, max: {}, total: {}".format(min(receivedMessages),max(receivedMessages),sum(receivedMessages)))
+    logger.info("receivedMessages min: {}, max: {}, total: {}".format(min(receivedMessages), max(receivedMessages),
+                                                                      sum(receivedMessages)))
     logger.info("sentMessages {}".format(sentMessages))
-    logger.info("sentMessages min: {}, max: {}, total: {}".format(min(sentMessages),max(sentMessages),sum(receivedMessages)))
+    logger.info(
+        "sentMessages min: {}, max: {}, total: {}".format(min(sentMessages), max(sentMessages), sum(receivedMessages)))
 
     # dump data into gnuplot format
-    utils.dumpAsGnuplot([receivedMessages, sentMessages], dumpPath + '/messages-' + str(runId) + '.gpData', ['#receivedMessages sentMessages'])
+    utils.dumpAsGnuplot([receivedMessages, sentMessages], dumpPath + '/messages-' + str(runId) + '.gpData',
+                        ['#receivedMessages sentMessages'])
 
     # dump data for later processing
     with open(dumpPath + '/dumps-' + str(runId) + '.obj', 'w') as f:
         cPickle.dump(receivedMessages, f)
         cPickle.dump(sentMessages, f)
+
 
 def createNode():
     # maintain the node state as a list with the required variables
@@ -97,13 +104,13 @@ def createNode():
     global MSGS_RECEIVED
     global MSGS_SENT
 
-    CURRENT_CYCLE, MSGS_RECEIVED, MSGS_SENT = 0,1,2
+    CURRENT_CYCLE, MSGS_RECEIVED, MSGS_SENT = 0, 1, 2
 
-    return [ 0, 0 , 0 ]
+    return [0, 0, 0]
 
 
 def configure(config):
-    global nbNodes, nbCycles, probBroadcast, nodeState, nodeCycle 
+    global nbNodes, nbCycles, probBroadcast, nodeState, nodeCycle
 
     IS_CHURN = config.get('CHURN', False)
     if IS_CHURN:
@@ -126,7 +133,7 @@ def configure(config):
     except:
         latencyTable = None
         latencyValue = int(latencyTablePath)
-        logger.warn('Using constant latency value: {}'.format(latencyValue) ) 
+        logger.warn('Using constant latency value: {}'.format(latencyValue))
 
     latencyTable = utils.checkLatencyNodes(latencyTable, nbNodes, latencyValue)
     latencyDrift = eval(config['LATENCY_DRIFT'])
@@ -144,10 +151,9 @@ def configure(config):
     sim.init(nodeCycle, nodeDrift, latencyTable, latencyDrift)
 
 
-
 if __name__ == '__main__':
 
-    #setup logger
+    # setup logger
     logger = logging.getLogger(__file__)
     logger.setLevel(logging.DEBUG)
     console = logging.StreamHandler()
@@ -164,22 +170,22 @@ if __name__ == '__main__':
     if LOG_TO_FILE:
         if not os.path.exists("logs/"):
             os.makedirs("logs/")
-            #logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG, filename='logs/echo.log', filemode='w')
+            # logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG, filename='logs/echo.log', filemode='w')
     dumpPath = sys.argv[1]
     confFile = dumpPath + '/conf.yaml'
     runId = int(sys.argv[2])
     f = open(confFile)
 
-    #load configuration file
+    # load configuration file
     configure(yaml.load(f))
     logger.info('Configuration done')
 
-    #start simulation
+    # start simulation
     init()
     logger.info('Init done')
-    #run the simulation
+    # run the simulation
     sim.run()
     logger.info('Run done')
-    #finish simulation, compute stats
+    # finish simulation, compute stats
     wrapup()
     logger.info("That's all folks!")
