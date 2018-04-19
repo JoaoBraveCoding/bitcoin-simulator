16f3dc731a8e85522c7b7f1e62051cf7188ebe54
JoaoBraveCoding <joao.marcal12@hotmail.com>
2018-04-19 11:30:18 +0100

    Implementing parent block

--- echo.py
+++ echo.py
@@ -1,7 +1,7 @@
 # Sample simulator demo
 # Miguel Matos - miguel.marques.matos@tecnico.ulisboa.pt
 # (c) 2012-2018
-
+import time
 from collections import defaultdict
 import math
 import random
@@ -31,6 +31,26 @@ def init():
     #    sim.schedulleExecutionFixed(MEASURE_X, nodeCycle * (c + 1))
 
 
+def get_highest_block(myself):
+    global nodeState
+
+    highest_block = None
+    for tpl in reversed(nodeState[myself][RECEIVED_BLOCKS]):
+        if highest_block is None or tpl[2] > highest_block[2] \
+                or (tpl[2] == highest_block[2] and tpl[3] > highest_block[3]):
+            highest_block = tpl
+
+    return highest_block
+
+
+def getBlock(myself, block_id):
+    item = None
+    for item in nodeState[myself][RECEIVED_BLOCKS]:
+        if item[0] == block_id:
+            return item
+    return None
+
+
 def CYCLE(myself):
     global nodeState, block_id, max_block_number
 
@@ -48,82 +68,106 @@ def CYCLE(myself):
     if nodeState[myself][CURRENT_CYCLE] < nbCycles:
         sim.schedulleExecution(CYCLE, myself)
 
-    # select random node to send message
-    # assume global view
-    if random.random() <= probBroadcast and block_id < max_block_number:
-        nodeState[myself][RECEIVED_BLOCKS].append(block_id)
+    # If the node can generate a block
+    if random.random() <= probBroadcast and (max_block_number == 0 or block_id < max_block_number):
+        # First block or
+        # Not first block which means getting highest block to be the parent
+        highest_block = None
+        if not nodeState[myself][RECEIVED_BLOCKS]:
+            new_block = (block_id, -1, 0, time.time())
+        else:
+            highest_block = get_highest_block(myself)
+            new_block = (block_id, highest_block[0], highest_block[2] + 1, time.time())
+
+        # Store the new block
+        nodeState[myself][RECEIVED_BLOCKS].append(new_block)
+
+        # Broadcast it
         for target in nodeState[myself][NEIGHBOURHOOD]:
-            sim.send(INV, target, myself, "hello, i am {} and I have this header".format(myself), block_id)
-            nodeState[myself][BLOCKS_AVAILABILITY].setdefault(target, []).append(block_id)
+            if highest_block is not None and highest_block in nodeState[myself][BLOCKS_AVAILABILITY][target]:
+                sim.send(CMPCTBLOCK, target, myself, "Here it is the most recent block".format(myself), new_block)
+                nodeState[myself][BLOCKS_AVAILABILITY].setdefault(target, []).append(block_id)
+            else:
+                sim.send(INV, target, myself, "hello, i am {} and I have this header".format(myself), block_id)
             nodeState[myself][MSGS_SENT] += 1
         block_id += 1
 
 
 def INV(myself, source, msg1, block_id):
     global nodeState
-    nodeState[myself][BLOCKS_AVAILABILITY].setdefault(source, []).append(block_id)
+    nodeState[myself][BLOCKS_AVAILABILITY].setdefault(source, []).append((block_id, 0, 0, 0))
 
     # TODO it does send inventory not headers
     logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, block_id))
     nodeState[myself][MSGS_RECEIVED] += 1
-    if block_id not in nodeState[myself][RECEIVED_BLOCKS]:
+    if block_id not in [tpl[0] for tpl in nodeState[myself][RECEIVED_BLOCKS]]:
         sim.send(GETHEADERS, source, myself, "Give me your headers", block_id)
 
 
-def GETHEADERS(myself, source, msg1, msg2):
-    global nodeState
-
-    logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, msg2))
-    nodeState[myself][MSGS_RECEIVED] += 1
-    sim.send(HEADERS, source, myself, "Here are my headers", msg2)
-
-
-def HEADERS(myself, source, msg1, msg2):
-    global nodeState
-
-    logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, msg2))
-    nodeState[myself][MSGS_RECEIVED] += 1
-    sim.send(GETDATA, source, myself, "Give me these blocks", msg2)
-
-
-def GETDATA(myself, source, msg1, msg2):
-    global nodeState
-
-    logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, msg2))
-    nodeState[myself][MSGS_RECEIVED] += 1
-    sim.send(BLOCK, source, myself, "These are the blocks requested", msg2)
-    nodeState[myself][BLOCKS_AVAILABILITY].setdefault(source, []).append(msg2)
-
-
-def BLOCK(myself, source, msg1, block_id):
+def GETHEADERS(myself, source, msg1, block_id):
     global nodeState
 
     logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, block_id))
     nodeState[myself][MSGS_RECEIVED] += 1
-    PROCESSBLOCK(myself, source, block_id)
+    sim.send(HEADERS, source, myself, "Here are my headers", block_id)
 
 
-def PROCESSBLOCK(myself, source, block_id):
-    if block_id not in nodeState[myself][RECEIVED_BLOCKS]:
-        nodeState[myself][RECEIVED_BLOCKS].append(block_id)
+def HEADERS(myself, source, msg1, block_id):
+    global nodeState
+
+    logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, block_id))
+    nodeState[myself][MSGS_RECEIVED] += 1
+    sim.send(GETDATA, source, myself, "Give me these blocks", block_id)
+
+
+def GETDATA(myself, source, msg1, block_id):
+    global nodeState
+
+    logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, block_id))
+    nodeState[myself][MSGS_RECEIVED] += 1
+    block_to_send = getBlock(myself, block_id)
+    if block_to_send is not None:
+        sim.send(BLOCK, source, myself, "These are the blocks requested", block_to_send)
+        nodeState[myself][BLOCKS_AVAILABILITY].setdefault(source, []).append(block_to_send)
+    else:
+        logger.info("Node {} Received {} from {} with {} INVALID REQUEST".format(myself, msg1, source, block_id))
+
+
+def BLOCK(myself, source, msg1, block):
+    global nodeState
+
+    logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, block))
+    nodeState[myself][MSGS_RECEIVED] += 1
+    PROCESSBLOCK(myself, source, block)
+
+
+def CMPCTBLOCK(myself, source, msg1, block):
+    global nodeState
+
+    logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, block))
+    nodeState[myself][MSGS_RECEIVED] += 1
+    PROCESSBLOCK(myself, source, block)
+
+
+def PROCESSBLOCK(myself, source, block):
+    if block not in nodeState[myself][RECEIVED_BLOCKS]:
+        nodeState[myself][RECEIVED_BLOCKS].append(block)
         for target in nodeState[myself][NEIGHBOURHOOD]:
             if target == source:
                 continue
-            if target in nodeState[myself][BLOCKS_AVAILABILITY] and block_id-1 in nodeState[myself][BLOCKS_AVAILABILITY][target]:
-                sim.send(CMPCTBLOCK, target, myself, "Here it is the most recent block".format(myself), block_id)
-                nodeState[myself][BLOCKS_AVAILABILITY].setdefault(target, []).append(block_id)
+
+            if target in nodeState[myself][BLOCKS_AVAILABILITY] and check_availability(myself, target, block[0]):
+                sim.send(CMPCTBLOCK, target, myself, "Here it is the most recent block".format(myself), block)
+                nodeState[myself][BLOCKS_AVAILABILITY].setdefault(target, []).append(block)
             else:
-                sim.send(INV, target, myself, "hello, i am {} and I have this headers".format(myself), block_id)
+                sim.send(INV, target, myself, "hello, i am {} and I have this headers".format(myself), block[0])
             nodeState[myself][MSGS_SENT] += 1
 
-
-def CMPCTBLOCK(myself, source, msg1, block_id):
-    global nodeState
-
-    logger.info("Node {} Received {} from {} with {}".format(myself, msg1, source, block_id))
-    nodeState[myself][MSGS_RECEIVED] += 1
-    PROCESSBLOCK(myself, source, block_id)
-
+def check_availability(myself, target, block_id):
+    for tpl in reversed(nodeState[myself][BLOCKS_AVAILABILITY][target]):
+        if tpl[0] == block_id:
+            return True
+    return False
 
 def GETBLOCKTXN(myself, source, msg1, msg2):
     global nodeState
@@ -148,6 +192,7 @@ def wrapup():
     receivedMessages = map(lambda x: nodeState[x][MSGS_RECEIVED], nodeState)
     sentMessages = map(lambda x: nodeState[x][MSGS_SENT], nodeState)
     receivedBlocks = map(lambda x: nodeState[x][RECEIVED_BLOCKS], nodeState)
+    sum_received_blocks = map(lambda x: map(lambda y: receivedBlocks[x][y][0], xrange(len(receivedBlocks[x]))), nodeState)
 
     # gather some stats, see utils for more functions
     logger.info("receivedMessages {}".format(receivedMessages))
@@ -158,9 +203,9 @@ def wrapup():
         "sentMessages min: {}, max: {}, total: {}".format(min(sentMessages), max(sentMessages), sum(receivedMessages)))
 
     # dump data into gnuplot format
-    utils.dumpAsGnuplot([receivedMessages, sentMessages, receivedBlocks],
+    utils.dumpAsGnuplot([receivedMessages, sentMessages, sum_received_blocks, receivedBlocks],
                         dumpPath + '/messages-' + str(runId) + '.gpData',
-                        ['receivedMessages sentMessages receivedBlocks'])
+                        ['receivedMessages sentMessages sum_received_blocks receivedBlocks'])
 
     # dump data for later processing
     with open(dumpPath + '/dumps-' + str(runId) + '.obj', 'w') as f:
