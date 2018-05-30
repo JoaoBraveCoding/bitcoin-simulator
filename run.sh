73e7edef9d53bc228615caea5e54e268156d7bcf
JoaoBraveCoding <joao.marcal12@hotmail.com>
2018-05-31 00:15:08 +0100

    Updated run script

--- run.sh
+++ run.sh
@@ -2,44 +2,38 @@
 # chmod 755 run.sh
 # run
 runId=1
-cycles=0
-tpNodes=0
 i=0
-filename=250-2-0
 
 if [ -z "$1" ]
 then
   echo "Include number of times to run cycle"
   exit -1
 fi
+if [ -z "$2" ]
+then
+  echo "Input filename"
+  exit -1
+fi
+
 
 
 cycles=$1
-((cycles=cycles*3-1))
+filename=$2
+pypy echo.py conf_echo/ 1 -sn True
+(( runId=runId+1 ))
 while [ "$i" -le "$cycles" ]
 do
-  if [ "$tpNodes" -eq "0" ]
-  then
-    if [ "$i" -eq "0" ]
-    then
-      if [ -z "$2" ]
-      then
-        pypy echo.py conf_echo/ 1 -sn True -tn $tpNodes
-      else
-        pypy echo.py conf_echo/ $runId -ln $filename -tn $tpNodes
-      fi
-    else
-      pypy echo.py conf_echo/ $runId -ln $filename -tn $tpNodes
-    fi
-  else
-    pypy echo.py conf_echo/ $runId -ln $filename -tn $tpNodes
-  fi
-
-  (( runId=runId+1 ))
-  (( i=i+1))
-  (( tpNodes=tpNodes-1))
-  if [ "$tpNodes" -eq "-1" ]
-  then
-    ((tpNodes=2))
-  fi
+    pypy echo.py conf_echo/ $runId -ln $filename -tn 2
+    (( runId=runId+1 )) 
+    pypy echo.py conf_echo/ $runId -ln $filename -tn 2 -rn 0
+    (( runId=runId+1 ))
+    pypy echo.py conf_echo/ $runId -ln $filename -tn 2 -rn 1
+    (( runId=runId+1 ))
+    pypy echo.py conf_echo/ $runId -ln $filename -tn 1
+    (( runId=runId+1 ))
+    pypy echo.py conf_echo/ $runId -ln $filename -tn 1 -rn 0
+    (( runId=runId+1 ))
+    pypy echo.py conf_echo/ $runId -ln $filename -tn 0
+    (( runId=runId+1 ))
+    (( i=i+1 ))
 done
